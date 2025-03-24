54a4764adf9e74bbeebcf7b8317bb01fbc98dc43
Anatoly Chernov <chertoly@gmail.com>
2025-03-25 00:03:36 +0200

    android: add ~/archive/android/android-studio/bin to PATH

--- zsh/zdotdir/android.sh
+++ zsh/zdotdir/android.sh
@@ -3,4 +3,5 @@ if [[ -d "$HOME/Android" ]]; then
   export PATH=$PATH:$ANDROID_HOME/emulator
   export PATH=$PATH:$ANDROID_HOME/platform-tools
   export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+  export PATH=$PATH:~/archive/android/android-studio/bin
 fi
