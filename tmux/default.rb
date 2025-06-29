8531d39e64bcddac3687232de7353a5be035ac61
Anatoly Chernov <chertoly@gmail.com>
2025-06-29 21:17:56 +0300

    tmux: delete tmux-fingers

--- tmux/default.rb
+++ tmux/default.rb
@@ -8,8 +8,6 @@ remote_file "#{HOME}/.config/tmux/tmux.conf" do
   source 'tmux.conf'
 end
 
-include_recipe 'pane_movements'
 include_recipe 'scripts'
-
+include_recipe 'pane_movements'
 include_recipe 'session_wizard'
-include_recipe 'fingers'
--- tmux/fingers.rb
+++
@@ -1,3 +0,0 @@
-git "#{TMUX_SOURCES}/tmux-fingers" do
-  repository 'https://github.com/Morantron/tmux-fingers'
-end
