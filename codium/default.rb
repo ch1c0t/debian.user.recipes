b678817361950e75250e1f2855e358ee37440904
Anatoly Chernov <chertoly@gmail.com>
2025-12-18 21:14:24 +0200

    code: install extensions

--- codium/default.rb
+++ codium/default.rb
@@ -1,5 +1,18 @@
 HOME = ENV['HOME']
 
+link "#{HOME}/.local/bin/code" do
+  to '/usr/bin/codium'
+end
+
 remote_file "#{HOME}/.config/VSCodium/User/settings.json" do
   source 'settings.json'
+end
+
+extensions = [
+  'DanielX.civet',
+  'asvetliakov.vscode-neovim',
+]
+
+extensions.each do |name|
+  execute "code --install-extension #{name}"
 end
