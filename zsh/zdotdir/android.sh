a351d418a1eac80273de434c95381a14771a2dcc
Anatoly Chernov <chertoly@gmail.com>
2025-12-22 04:07:01 +0200

    android: get platform-tools
    https://garden.pacia.tech/suckless-androidsdk-setup.html#platform-tools
    
    sdkmanager --install 'platform-tools'

--- zsh/zdotdir/android.sh
+++ zsh/zdotdir/android.sh
@@ -1,7 +1,6 @@
 if [[ -d "$HOME/android.sdk" ]]; then
   export ANDROID_USER_HOME="$HOME/.android"
   export ANDROID_HOME=$HOME/android.sdk
-  export PATH=$PATH:$ANDROID_HOME/platform-tools
   export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 
   if [ -d "$ANDROID_HOME/build-tools" ]; then
@@ -12,5 +11,9 @@ if [[ -d "$HOME/android.sdk" ]]; then
     )
 
     export PATH="$ANDROID_HOME/build-tools/$build_tools:$PATH"
+  fi
+
+  if [ -d "$ANDROID_HOME/platform-tools" ]; then
+    export PATH=$PATH:$ANDROID_HOME/platform-tools
   fi
 fi
