81ce545e4d9ba3165c076a3655de8b6e2e55eeee
Anatoly Chernov <chertoly@gmail.com>
2025-12-22 04:00:31 +0200

    android: get build-tools
    https://garden.pacia.tech/suckless-androidsdk-setup.html#build-tools
    https://developer.android.com/tools/sdkmanager
    https://developer.android.com/tools/releases/cmdline-tools
    https://developer.android.com/tools/variables
    
    sdkmanager --install "build-tools;34.0.0"

--- android/commandlinetools/default.rb
+++ android/commandlinetools/default.rb
@@ -6,10 +6,20 @@ directory ANDROID_ARCHIVE
 download_command = """
 curl -fsSL \
   --output-dir #{ANDROID_ARCHIVE} \
-  --output android-commandlinetools.zip \
+  --output commandlinetools.zip \
   https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip
 """
 
 execute download_command do
-  not_if "test -f #{ANDROID_ARCHIVE}/android-commandlinetools.zip"
+  not_if "test -f #{ANDROID_ARCHIVE}/commandlinetools.zip"
+end
+
+execute "unzip #{ANDROID_ARCHIVE}/commandlinetools.zip -d #{ANDROID_ARCHIVE}" do
+  not_if "test -d #{ANDROID_ARCHIVE}/cmdline-tools"
+end
+
+ANDROID_DIR_FOR_LATEST_CMDLINETOOLS = "#{HOME}/android.sdk/cmdline-tools"
+directory ANDROID_DIR_FOR_LATEST_CMDLINETOOLS
+execute "cp -a #{ANDROID_ARCHIVE}/cmdline-tools #{ANDROID_DIR_FOR_LATEST_CMDLINETOOLS}/latest" do
+  not_if "test -d #{ANDROID_DIR_FOR_LATEST_CMDLINETOOLS}/latest"
 end
--- zsh/zdotdir/android.sh
+++ zsh/zdotdir/android.sh
@@ -1,7 +1,16 @@
-if [[ -d "$HOME/Android" ]]; then
-  export ANDROID_HOME=$HOME/Android/Sdk
-  export PATH=$PATH:$ANDROID_HOME/emulator
+if [[ -d "$HOME/android.sdk" ]]; then
+  export ANDROID_USER_HOME="$HOME/.android"
+  export ANDROID_HOME=$HOME/android.sdk
   export PATH=$PATH:$ANDROID_HOME/platform-tools
   export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
-  export PATH=$PATH:~/archive/android/android-studio/bin
+
+  if [ -d "$ANDROID_HOME/build-tools" ]; then
+    build_tools=$(
+      command ls "$ANDROID_HOME/build-tools" |
+        sort --version-sort --reverse |
+        head -n 1
+    )
+
+    export PATH="$ANDROID_HOME/build-tools/$build_tools:$PATH"
+  fi
 fi
