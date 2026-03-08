62bb44608b10abdfbf77a4fc0b29f5968ce51177
Anatoly Chernov <chertoly@gmail.com>
2026-03-08 22:27:58 +0200

    download: support extracting files from ISOs
    https://wiki.debian.org/DebianInstaller/Preseed/EditIso
    https://wiki.debian.org/RepackBootableISO
    https://wiki.debian.org/ManipulatingISOs
    https://www.linuxquestions.org/questions/debian-26/remastering-a-debian-live-operating-system-4175714801/

--- helpers/download.rb
+++ helpers/download.rb
@@ -8,20 +8,35 @@ params
       it will be a path inside of ~/.local/state/mitamae/downloads
       its filename will be derived from :name
   :extract_to String
-    The directory to extract files from the downloaded file.
+    The directory to extract files to.
+    If not specified:
+      it will be inside of the same directory as the downloaded file
+      its name will be derived
+        from the name of the downloaded file
+          by adding ".extract" to its end
+  :extract Boolean
+    Whether to extract.
+    If not specified:
+      it is true if :extract_to was specified
+      it is false otherwise
 =end
-define :download, to: nil, extract_to: nil do
+define :download, to: nil, extract_to: nil, extract: nil do
   url = params[:name]
   
   file_destination = params[:to]
   extract_directory = params[:extract_to]
 
+  downloads_directory = File.expand_path '~/.local/state/mitamae/downloads'
+  file_destination ||= begin
+                         file_name = url.split('/').last
+                         "#{downloads_directory}/#{file_name}"
+                       end
+
+  extract_directory ||= if params[:extract]
+                          "#{file_destination}.extract"
+                        end
+
   if url
-    downloads_directory = File.expand_path '~/.local/state/mitamae/downloads'
-    file_destination ||= begin
-                           file_name = url.split('/').last
-                           "#{downloads_directory}/#{file_name}"
-                         end
     file_destination_directory = File.dirname file_destination
     directory file_destination_directory
 
@@ -30,9 +45,14 @@ define :download, to: nil, extract_to: nil do
     end
 
     if extract_directory
-      directory extract_directory
       if url.end_with? '.tar.xz'
+        directory extract_directory
         execute "tar xf #{file_destination} -C #{extract_directory}"
+      end
+      if url.end_with? '.iso'
+        execute "xorriso -osirrox on -indev #{file_destination} -extract / #{extract_directory}" do
+          not_if "test -d #{extract_directory}"
+        end
       end
     end
   else
--- live/default.rb
+++ live/default.rb
@@ -1,3 +1,5 @@
 include_recipe '../helpers/download'
 
-download 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-13.3.0-amd64-gnome.iso'
+download 'https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-13.3.0-amd64-gnome.iso' do
+  extract true
+end
