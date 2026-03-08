50a7e7674512ebfdc08b3c79924501fe2ce5b53e
Anatoly Chernov <chertoly@gmail.com>
2026-03-08 18:03:00 +0200

    download: make passing the file destination optional

--- helpers/download.rb
+++ helpers/download.rb
@@ -1,44 +1,38 @@
+=begin
+params
+  :name String
+    A URL. An address to download the file from.
+  :to String
+    The file path to save the downloaded file to.
+    If not specified:
+      it will be a path inside of ~/.local/state/mitamae/downloads
+      its filename will be derived from :name
+  :extract_to String
+    The directory to extract files from the downloaded file.
+=end
 define :download, to: nil, extract_to: nil do
   url = params[:name]
   
   file_destination = params[:to]
-  path_destination = params[:extract_to]
-
-  unless file_destination or path_destination
-    message = <<~S
-    The required params were not passed.
-    The `download` helper expects either
-
-      params[:to] String
-        The file path to save the downloaded file to.
-
-      or
-
-      params[:extract_to] String
-        The path to the directory to extract the downloaded archive.
-        The directory will be created if it does not exist.
-
-      or both of the above.
-    S
-    fail message
-  end
+  extract_directory = params[:extract_to]
 
   if url
-    tmp_directory = File.expand_path '~/.local/state/mitamae/download'
+    downloads_directory = File.expand_path '~/.local/state/mitamae/downloads'
     file_destination ||= begin
                            file_name = url.split('/').last
-                           "#{tmp_directory}/#{file_name}"
+                           "#{downloads_directory}/#{file_name}"
                          end
-    file_destination_path = File.dirname file_destination
-    directory file_destination_path
+    file_destination_directory = File.dirname file_destination
+    directory file_destination_directory
+
     execute "wcurl --output #{file_destination} #{url}" do
       not_if "test -f #{file_destination}"
     end
 
-    if path_destination
-      directory path_destination
+    if extract_directory
+      directory extract_directory
       if url.end_with? '.tar.xz'
-        execute "tar xf #{file_destination} -C #{path_destination}"
+        execute "tar xf #{file_destination} -C #{extract_directory}"
       end
     end
   else
