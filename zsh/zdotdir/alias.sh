07c6e3e68e53fd77111b3e92acba0c68702e38ac
Anatoly Chernov <chertoly@gmail.com>
2026-06-20 01:28:51 +0300

    zsh: add flatpak aliases

--- zsh/zdotdir/alias.sh
+++ zsh/zdotdir/alias.sh
@@ -1,4 +1,5 @@
 . $(dirname $0)/alias/editor.sh
 . $(dirname $0)/alias/systemd.sh
 . $(dirname $0)/alias/ruby.sh
+. $(dirname $0)/alias/flatpak.sh
 . $(dirname $0)/alias/other.sh
---
+++ zsh/zdotdir/alias/flatpak.sh
@@ -0,0 +1,9 @@
+alias fp='flatpak --user'
+
+alias fpin='flatpak --user install'
+alias fpli='flatpak --user list'
+alias fpru='flatpak --user run'
+alias fpse='flatpak --user search'
+
+alias fpup='flatpak --user update'
+alias fp-updates='flatpak --user remote-ls --updates'
