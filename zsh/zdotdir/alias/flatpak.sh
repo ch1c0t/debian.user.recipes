a32646c71ec4f3b2dcb8c0fb53d3427d76076ad3
Anatoly Chernov <chertoly@gmail.com>
2026-06-21 00:48:59 +0300

    flatpak: add more aliases

--- zsh/zdotdir/alias/flatpak.sh
+++ zsh/zdotdir/alias/flatpak.sh
@@ -6,4 +6,9 @@ alias fpru='flatpak --user run'
 alias fpse='flatpak --user search'
 
 alias fpup='flatpak --user update'
+alias fplo='flatpak --user remote-info --log'
 alias fp-updates='flatpak --user remote-ls --updates'
+alias fp-permissions='flatpak --user info --show-permissions'
+
+alias fps='flatpak ps'
+alias fpen='flatpak enter'
