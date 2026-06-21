a1f52c71e773fc2b79ee0b6c2de6aee3f4f5a20e
Anatoly Chernov <chertoly@gmail.com>
2026-06-21 20:28:30 +0300

    flatpak: add fplia alias

--- zsh/zdotdir/alias/flatpak.sh
+++ zsh/zdotdir/alias/flatpak.sh
@@ -1,9 +1,10 @@
 alias fp='flatpak --user'
 
 alias fpin='flatpak --user install'
-alias fpli='flatpak --user list'
 alias fpru='flatpak --user run'
 alias fpse='flatpak --user search'
+alias fpli='flatpak --user list'
+alias fplia='flatpak --user list --app'
 
 alias fpup='flatpak --user update'
 alias fplo='flatpak --user remote-info --log'
