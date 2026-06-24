bfda654548e6a68e6eed8691cf7d4f800ee02003
Anatoly Chernov <chertoly@gmail.com>
2026-06-24 23:01:19 +0300

    zsh: use add-zsh-hook precmd

--- zsh/zdotdir/prompt.sh
+++ zsh/zdotdir/prompt.sh
@@ -1,10 +1,14 @@
 setopt PROMPT_SUBST
 
 autoload -Uz vcs_info
-precmd() {
+autoload -Uz add-zsh-hook
+
+print-prompt-header() {
   vcs_info
   print -rP "%F{green}●%f %(!.%F{red}%n%f.%n) %F{211}%3~%f ${vcs_info_msg_0_}"
 }
+
+add-zsh-hook precmd print-prompt-header
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' check-for-changes true
