e502903dded5b785ae14a736d8c1dc2a98ce955a
Anatoly Chernov <chertoly@gmail.com>
2026-06-29 22:49:25 +0300

    lima: enable zsh completion
    https://lima-vm.io/docs/usage/

--- zsh/zdotdir/completion.sh
+++ zsh/zdotdir/completion.sh
@@ -1,2 +1,7 @@
 autoload -Uz compinit
 compinit -d ~/.cache/zsh/zcompdump-$ZSH_VERSION
+
+# How it works: +commands[git] returns 1 if git exists in your $PATH and 0 if it does not. The arithmetic evaluation (( ... )) treats 1 as success (true) and 0 as failure (false).
+if (( $+commands[limactl] )); then
+  source <(limactl completion zsh)
+fi
