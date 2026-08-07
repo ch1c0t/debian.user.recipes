0858f606b06d39fa39d5b42ce3bf1f2a7f5e6a52
Anatoly Chernov <chertoly@gmail.com>
2026-08-07 18:35:51 +0300

    lima: configure a shared directory
    https://www.metachris.dev/2025/11/sandbox-your-ai-dev-tools-a-practical-guide-for-vms-and-lima/

--- lima/default.rb
+++ lima/default.rb
@@ -12,3 +12,16 @@ end
 links_in "#{HOME}/.local/bin" do
   to_each_file_in "#{HOME}/.local/state/mitamae/downloads/lima-#{version}-Linux-x86_64.tar.gz.extract/bin"
 end
+
+SHARED_DIRECTORY = "#{HOME}/shared"
+directory SHARED_DIRECTORY
+
+LIMA_CONFIG = "#{HOME}/.lima/_config"
+file "#{LIMA_CONFIG}/default.yml" do
+  content <<~S
+    mounts:
+    - location: "~/shared"
+      mountPoint: "{{.Home}}/shared"
+      writable: true
+  S
+end
