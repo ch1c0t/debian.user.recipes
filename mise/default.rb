38f7aaa56f753271e55c754651283b2946551442
Anatoly Chernov <chertoly@gmail.com>
2026-08-31 23:46:28 +0300

    mise: install tools in mise/default.rb

--- mise/default.rb
+++ mise/default.rb
@@ -3,3 +3,9 @@ HOME = ENV['HOME']
 execute "curl https://mise.run | sh" do
   not_if "test -f #{HOME}/.local/bin/mise"
 end
+
+execute '~/.local/bin/mise use -g crystal'
+execute '~/.local/bin/mise use -g node@24'
+execute '~/.local/bin/mise use -g ruby@3.4'
+execute '~/.local/bin/mise use -g python@3.14'
+execute '~/.local/bin/mise use -g uv@0.11'
