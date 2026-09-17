c6df65cda0b6d62923a72257f98f4856bf4102d8
Anatoly Chernov <chertoly@gmail.com>
2026-09-17 20:07:15 +0300

    zsh: export LAVINMQ_AMQP_UNIXSOCKET

--- zsh/zdotdir/env.sh
+++ zsh/zdotdir/env.sh
@@ -3,3 +3,4 @@ export WORDCHARS=''
 export EDITOR="vim"
 export TERMINAL="sakura"
 export REDIS_UNIXSOCKET="$HOME/.local/share/redis/socket"
+export LAVINMQ_AMQP_UNIXSOCKET="$HOME/.local/share/lavinmq/amqp.sock"
